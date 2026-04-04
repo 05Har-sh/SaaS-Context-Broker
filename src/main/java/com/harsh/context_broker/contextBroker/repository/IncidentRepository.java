1217794419b08b955bfffbe25882fad6ba3f5975
Harsh Pardeshi <pardeshiharsh005@gmail.com>
2026-04-04 23:55:01 +0530

    implement pagination and sorting

--- src/main/java/com/harsh/context_broker/contextBroker/repository/IncidentRepository.java
+++ src/main/java/com/harsh/context_broker/contextBroker/repository/IncidentRepository.java
@@ -2,10 +2,12 @@ package com.harsh.context_broker.contextBroker.repository;
 
 import com.harsh.context_broker.contextBroker.entity.IncidentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface IncidentRepository extends JpaRepository<IncidentEntity, Long> {
+public interface IncidentRepository extends JpaRepository<IncidentEntity, Long>,
+        JpaSpecificationExecutor<IncidentEntity> {
     Optional<IncidentEntity> findByIncidentKey(String incidentKey);
     long count();
 
---
+++ src/main/java/com/harsh/context_broker/contextBroker/specification/IncidentSpecification.java
@@ -0,0 +1,25 @@
+package com.harsh.context_broker.contextBroker.specification;
+
+import com.harsh.context_broker.contextBroker.entity.IncidentEntity;
+import com.harsh.context_broker.contextBroker.model.JiraStatus;
+import com.harsh.context_broker.contextBroker.model.Severity;
+import org.springframework.data.jpa.domain.Specification;
+
+public class IncidentSpecification {
+    public static Specification<IncidentEntity> hasSeverity(Severity severity){
+        return (root, query, cb) ->
+                severity == null ? null : cb.equal(root.get("severity"), severity);
+    }
+
+    public static Specification<IncidentEntity> hasAssignedTo(String assignedTo){
+        return (root, query, cb) ->
+                assignedTo == null ? null :
+                        cb.equal(cb.lower(root.get("assignedTo")), assignedTo.toLowerCase());
+    }
+
+    public static Specification<IncidentEntity> hasJiraStatus(JiraStatus status){
+        return (root, query, cb) ->
+                status == null ? null :
+                        cb.equal(root.get("jiraStatus"), status);
+    }
+}
